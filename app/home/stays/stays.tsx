38a5838a58abe5476ae68c5e9457b55a1b464ffa
BlazBrace <blazbrace@gmail.com>
2023-10-05 08:16:35 +0530

    Connect Firestore

--- app/home/stays/stays.tsx
+++ app/home/stays/stays.tsx
@@ -1,13 +1,33 @@
+"use client"
 import Image from "next/image";
 import cardList from "./data"
 import img from "@/assets/imgSample.webp"
+import { useEffect, useState } from "react";
+import { onSnapshot, query, where, collection } from "firebase/firestore";
+import { db } from "@/app/authentication/firebase";
+
 
 export default function Stays() {
+
+  const [stays, setStays] = useState<{ id: string; name: string; desc: string }[]>([]);
+
+  useEffect(() => {
+    const q = query(collection(db, "hotels"), where("amenities", "==", "ac"));
+
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      setStays(
+        querySnapshot.docs.map((doc) => ({ id: doc.id, name: doc.data().name, desc: doc.data().desc}))
+      );
+    });
+
+    return () => unsubscribe(); // Cleanup when the component unmounts
+  }, []);
+
   return(
     <main className="container mx-auto py-8 px-8">
       <h1 className="text-4xl font-bold pb-3">Find Best Stays!</h1>
       <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1 gap-6">
-        {cardList.map(card => (
+        {stays.map(card => (
           <div
           className="shadow-lg rounded-lg" 
           key={card.id} >
@@ -16,8 +36,8 @@ export default function Stays() {
             src={img}
             alt="Picture of the author"></Image>
             <div className="p-5">
-            <h3 className="text-3xl font-bold mb-3">{card.title}</h3>
-            <p className="text-lg font-normal">{card.text}</p>
+            <h3 className="text-3xl font-bold mb-3">{card.name}</h3>
+            <p className="text-lg font-normal">{card.desc}</p>
             </div>
           </div>
         ))}
--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,13 @@ import { redirect } from 'next/navigation';
 import Register from './authentication/register/page';
 import HomePage from './home/page';
 import Login from './authentication/signin/page';
+import Stays from './home/stays/stays';
 
 
 export default function Home() {
   
   return (
-    <Login/>
+    <HomePage/>
   )
 }
 
